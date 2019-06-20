b71be90cbf001ef40fd4e2bf5aafc9fa704f892c
Daniel Pereira de Souza <daniel_mfn@homail.com>
2019-06-20 18:50:56 -0300

    alteração do template

--- src/main/java/br/com/bitscamp/chatbot/indexController.java
+++ src/main/java/br/com/bitscamp/chatbot/indexController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class indexController{
     @RequestMapping("/")
     public String index(){
-        return "index";
+        return "base";
     }
 }
