22661717e4f31c126799ff69f46a70fbb181f609
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-06-23 00:57:46 -0300

    index template

--- src/main/java/br/com/bitscamp/chatbot/controller/indexResource.java
+++ src/main/java/br/com/bitscamp/chatbot/controller/indexResource.java
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class indexResource {
+
     @RequestMapping("/")
+    public String base(){
+        return "index";
+    }
+
+    @RequestMapping("/index.html")
     public String index(){
         return "index";
     }
