56a8fcaec828c49680fdb8f01d6f2e176c74c2ad
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-06-26 21:35:12 -0300

    Adicionando Spring security como depedencia

--- src/main/java/br/com/bitscamp/chatbot/model/Usuario.java
+++ src/main/java/br/com/bitscamp/chatbot/model/Usuario.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_usuario")
-public class Usuario implements Serializable{
+public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
