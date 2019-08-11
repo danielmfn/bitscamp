7d2c71431d11ce123f0ad7b24d04f7c8da03ebc0
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-08-11 00:17:37 -0300

    Teste Cors.

--- src/main/java/br/com/bitscamp/chatbot/controller/PagesResource.java
+++ src/main/java/br/com/bitscamp/chatbot/controller/PagesResource.java
@@ -1,5 +1,6 @@
 package br.com.bitscamp.chatbot.controller;
 import br.com.bitscamp.chatbot.enums.UrlEnum;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,44 +9,53 @@ public class PagesResource {
 
     String urlSistema = UrlEnum.GestaoFrontEnd.URL.getUrl();
 
+    @CrossOrigin
     @RequestMapping("/")
     public String index(){
         return urlSistema+"/index";
     }
 
+    @CrossOrigin
     @RequestMapping("/base")
     public String base(){
         return urlSistema+"/pages/base/base";
     }
 
+    @CrossOrigin
     @RequestMapping("/manter_usuario")
     public String usuario(){
         return urlSistema+"/pages/usuario/manter_usuario";
     }
 
+    @CrossOrigin
     @RequestMapping("/manter_categoria")
     public String categoriaUsuario(){
         return urlSistema+"/pages/usuario/manter_categoria";
     }
 
+    @CrossOrigin
     @RequestMapping("/manter_loja")
     public String loja(){
         return urlSistema+"/pages/loja/manter_loja";
     }
 
+    @CrossOrigin
     @RequestMapping("/manter_cliente")
     public String cliente(){
         return urlSistema+"/pages/cliente/manter_cliente";
     }
 
+    @CrossOrigin
     @RequestMapping("/manter_perfil")
     public String perfilCliente(){
         return urlSistema+"/pages/cliente/manter_perfil";
     }
 
+    @CrossOrigin
     @RequestMapping("/login")
     public String login() { return urlSistema+"/pages/autenticacao/login"; }
 
+    @CrossOrigin
     @RequestMapping("/logout")
     public String logout() { return urlSistema+"/pages/autenticacao/logout"; }
 
