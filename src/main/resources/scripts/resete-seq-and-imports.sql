7a8bad0fdb7c47866f4bfd0d1a3ace9af8f82160
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-08-16 17:44:45 -0300

    Ajustes parciais no h2 database.

--- src/main/resources/scripts/resete-seq-and-imports.sql
+++ src/main/resources/scripts/resete-seq-and-imports.sql
@@ -24,23 +24,28 @@ VALUES('Administrador'),
       ('Funcionário'),
       ('Desenvolvedor');
 
+INSERT INTO tb_estado(id_estado, estado, uf)
+VALUES(1, 'Santa Catarina', 'SC'),
+      (2, 'São Paulo', 'SP'),
+      (3, 'Distrito Federal', 'DF'),
+      (4, 'Paraná', 'PR');
 
 INSERT INTO tb_usuario(id_categoria_usuario, nome_usuario, email_usuario, cep_usuario, endereco_usuario, telefone_usuario, senha_usuario, id_perfil_cliente, municipio_usuario, id_estado)
 VALUES(1, 'Victória Andrade', 'victoria.andrade@bitscamp.com.br', 72451478, 'Rua 01', 61986352220, '', 1, 'Brasília', 4),
       (1, 'Guilherme Santos', 'guilherme.s354@gmail.com', 72451478, 'Rua 02', 61986352225, '', 2, 'Goiania', 2),
-      (2, 'Everton Rodrigues', 'h3v3rton@gmail.com', 72451414, 'Rua 03', 61986352214, '', 1, 'Brasília', 11),
-      (1, 'Telmo Silva', 'telmosilvah4@outlook.com', 72451419, 'Rua 04', 61986352217, '', 1, 'São Paulo', 7),
+      (2, 'Everton Rodrigues', 'h3v3rton@gmail.com', 72451414, 'Rua 03', 61986352214, '', 1, 'Brasília', 3),
+      (1, 'Telmo Silva', 'telmosilvah4@outlook.com', 72451419, 'Rua 04', 61986352217, '', 1, 'São Paulo', 2),
       (2, 'Marcelo Fernandes', 'm78@gmail.com', 72451357, 'Rua 05', 61986352240, '', 1, 'Brasília', 3),
       (2, 'Pedro Henrrique', 'pedro.henrrique@bitscamp.com.br', 72451365, 'Rua 06', 61986352257, '', 1, 'Brasília', 1),
-      (1, 'Helena Maria', 'h.elena@hotmail.com', 72451111, 'Rua 07', 61986352247, '', 1, 'Brasília', 10),
-      (2, 'Maria Eduarda', 'duda87@gmail.com', 72451225, 'Rua 08', 61986352365, '', 1, 'Santos', 8);
+      (1, 'Helena Maria', 'h.elena@hotmail.com', 72451111, 'Rua 07', 61986352247, '', 1, 'Brasília', 4),
+      (2, 'Maria Eduarda', 'duda87@gmail.com', 72451225, 'Rua 08', 61986352365, '', 1, 'Santos', 1);
 
 INSERT INTO tb_loja
 (cnpj_loja, nome_loja, telefone_loja, email_loja, municipio_loja, id_estado, cep_loja, endereco_loja)
-VALUES(78965412365411, 'Loja 01', 61986325487, 'comercial@loja1.com.br', 'Rio de Janeiro', 7, 72365531, 'Avenida 01'),
-      (78965412365422, 'Loja 02', 61986325488, 'comercial@loja2.com.br', 'Brasília', 5, 72365544, 'Avenida 02'),
+VALUES(78965412365411, 'Loja 01', 61986325487, 'comercial@loja1.com.br', 'Rio de Janeiro', 2, 72365531, 'Avenida 01'),
+      (78965412365422, 'Loja 02', 61986325488, 'comercial@loja2.com.br', 'Brasília', 4, 72365544, 'Avenida 02'),
       (78965412365433, 'Loja 03', 61986325489, 'comercial@loja3.com.br', 'Goiania', 3, 72365546, 'Avenida 03'),
-      (78965412365444, 'Loja 04', 61986325490, 'comercial@loja4.com.br', 'Belo Horizonte', 1, 72365521, 'Avenida 04'),
-      (78965412365455, 'Loja 05', 61986325491, 'comercial@loja5.com.br', 'São Paulo', 12, 72365511, 'Avenida 05');
+      (78965412365444, 'Loja 04', 61986325490, 'comercial@loja4.com.br', 'Belo Horizonte', 3, 72365521, 'Avenida 04'),
+      (78965412365455, 'Loja 05', 61986325491, 'comercial@loja5.com.br', 'São Paulo', 1, 72365511, 'Avenida 05');
 
 
---
+++ src/main/resources/scripts/schema.sql
@@ -0,0 +1,90 @@
+CREATE TABLE public.tb_categoria_usuario (
+                                             id_categoria_usuario bigserial NOT NULL,
+                                             categoria_usuario varchar(20) NOT NULL,
+                                             CONSTRAINT tb_categoria_usuario_categoria_un UNIQUE (categoria_usuario),
+                                             CONSTRAINT tb_categoria_usuario_id_categoria_usuario_pk PRIMARY KEY (id_categoria_usuario)
+);
+
+CREATE TABLE public.tb_estado (
+                                  id_estado bigserial NOT NULL,
+                                  estado varchar(50) NOT NULL,
+                                  uf varchar(2) NOT NULL,
+                                  CONSTRAINT tb_estado_id_estado_pk PRIMARY KEY (id_estado),
+                                  CONSTRAINT tb_estado_id_estado_un UNIQUE (id_estado)
+);
+
+
+CREATE TABLE public.tb_perfil_cliente (
+                                          id_perfil_cliente bigserial NOT NULL,
+                                          perfil_cliente varchar(20) NOT NULL,
+                                          CONSTRAINT tb_perfil_cliente_id_perfil_pk PRIMARY KEY (id_perfil_cliente),
+                                          CONSTRAINT tb_perfil_cliente_perfil_cliente_un UNIQUE (perfil_cliente)
+);
+
+CREATE TABLE public.tb_loja (
+                                id_loja bigserial NOT NULL,
+                                cnpj_loja int8 NOT NULL,
+                                nome_loja varchar(150) NOT NULL,
+                                telefone_loja int8 NULL,
+                                email_loja varchar(255) NULL,
+                                municipio_loja varchar(250) NULL,
+                                id_estado int8 NULL,
+                                cep_loja int8 NULL,
+                                endereco_loja varchar(250) NULL,
+                                CONSTRAINT tb_loja_cnpj_loja_un UNIQUE (cnpj_loja),
+                                CONSTRAINT tb_loja_id_loja_pk PRIMARY KEY (id_loja),
+                                CONSTRAINT tb_loja_id_estado_fk FOREIGN KEY (id_estado) REFERENCES tb_estado(id_estado) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE public.tb_usuario (
+                                   id_usuario bigserial NOT NULL,
+                                   id_categoria_usuario int8 NOT NULL,
+                                   nome_usuario varchar(70) NOT NULL,
+                                   email_usuario varchar(100) NOT NULL,
+                                   cep_usuario int8 NULL,
+                                   endereco_usuario varchar(50) NULL,
+                                   telefone_usuario int8 NULL,
+                                   senha_usuario varchar(50) NULL,
+                                   id_perfil_cliente int8 NULL,
+                                   municipio_usuario varchar NULL,
+                                   id_estado int8 NULL,
+                                   CONSTRAINT tb_usuario_id_usuario_pk PRIMARY KEY (id_usuario),
+                                   CONSTRAINT tb_usuario_id_categoria_usuario_fk FOREIGN KEY (id_categoria_usuario) REFERENCES tb_categoria_usuario(id_categoria_usuario) ON UPDATE CASCADE ON DELETE SET NULL,
+                                   CONSTRAINT tb_usuario_id_estado_fk FOREIGN KEY (id_estado) REFERENCES tb_estado(id_estado) ON UPDATE CASCADE ON DELETE SET NULL,
+                                   CONSTRAINT tb_usuario_id_perfil_cliente_fk FOREIGN KEY (id_perfil_cliente) REFERENCES tb_perfil_cliente(id_perfil_cliente) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE public.tb_indicacao_produto (
+                                             id_indicacao bigserial NOT NULL,
+                                             nome_produto varchar(50) NOT NULL,
+                                             categoria_produto varchar(25) NOT NULL,
+                                             valor_produto numeric(2) NOT NULL,
+                                             url_produto varchar NOT NULL,
+                                             id_usuario int8 NOT NULL,
+                                             dt_indicacao date NOT NULL,
+                                             id_loja int8 NOT NULL,
+                                             CONSTRAINT tb_indicacao_id_indicacao_id_usuario_dt_indicacao_pk PRIMARY KEY (id_indicacao, id_usuario, dt_indicacao),
+                                             CONSTRAINT tb_indicacao_produto_un UNIQUE (id_indicacao),
+                                             CONSTRAINT tb_indicacao_produto_id_loja_fk FOREIGN KEY (id_loja) REFERENCES tb_loja(id_loja),
+                                             CONSTRAINT tb_usuario_id_usuario_fk FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+);
+
+CREATE TABLE public.tb_log_comunicacao (
+                                           id_log_comunicacao bigserial NOT NULL,
+                                           id_usuario int8 NOT NULL,
+                                           dt_comunicacao date NOT NULL,
+                                           mensagem varchar(250) NULL,
+                                           CONSTRAINT tb_log_comunicacao_id_log_comunicacao_un UNIQUE (id_log_comunicacao),
+                                           CONSTRAINT tb_log_id_log_id_usuario_dt_comunicacao_pk PRIMARY KEY (id_log_comunicacao, id_usuario, dt_comunicacao),
+                                           CONSTRAINT tb_log_id_usuario_fk FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+);
+
+CREATE TABLE public.tb_feedback (
+                                    id_feedback bigserial NOT NULL,
+                                    id_indicacao int8 NOT NULL,
+                                    dt_feedback date NOT NULL,
+                                    mensagem varchar(250) NOT NULL,
+                                    CONSTRAINT tb_feedback_id_feedback_un UNIQUE (id_feedback),
+                                    CONSTRAINT tb_feedback_id_indicacao_id_feedback_pk PRIMARY KEY (id_indicacao, id_feedback),
+                                    CONSTRAINT tb_feedback_id_indicacao_fk FOREIGN KEY (id_indicacao) REFERENCES tb_indicacao_produto(id_indicacao)
+);
