79118a2d3cdcfeca0092248ea9764709f4995184
Daniel Pereira de Souza <daniel_mfn@hotmail.com>
2019-08-17 21:51:38 -0300

    Limpeza de imports desnecessários

--- src/main/java/br/com/bitscamp/chatbot/model/Loja.java
+++ src/main/java/br/com/bitscamp/chatbot/model/Loja.java
@@ -1,8 +1,5 @@
 package br.com.bitscamp.chatbot.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import java.io.Serializable;
 
 import javax.persistence.Column;
